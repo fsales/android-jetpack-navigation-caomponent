41cd563c38fe7b47fe9f448d571fe6452a35ae94
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-06-15 21:27:35 -0300

    configurando navigation component

---
+++ NavigationComponentApp/app/src/main/java/com/example/navigationcomponentapp/ui/start/StartFragment.kt
@@ -0,0 +1,38 @@
+package com.example.navigationcomponentapp.ui.start
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.navigationcomponentapp.R
+import kotlinx.android.synthetic.main.fragment_start.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class StartFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonNext.setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_profileFragment)
+        }
+    }
+
+
+}
