4a6bb12b64ee0925ef6c9e127b172d4c9770315e
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-06-15 21:45:10 -0300

    configurando a toolbar

--- NavigationComponentApp/app/src/main/java/com/example/navigationcomponentapp/MainActivity.kt
+++ NavigationComponentApp/app/src/main/java/com/example/navigationcomponentapp/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.navigationcomponentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.navigationcomponentapp.ui.profile.ProfileFragment
 import com.example.navigationcomponentapp.ui.start.StartFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(myToolbar)
+
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+
     }
 
 
